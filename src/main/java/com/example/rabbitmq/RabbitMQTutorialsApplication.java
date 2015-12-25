13043cea7ae4b1de919f6e9ae3b7ee3b8332a4c1
ajmalch <ajmalch@gmail.com>
2015-12-24 23:04:05 -0500

    Updated to Deploy WebServer

--- src/main/java/com/example/rabbitmq/RabbitMQTutorialsApplication.java
+++ src/main/java/com/example/rabbitmq/RabbitMQTutorialsApplication.java
@@ -4,7 +4,10 @@ package com.example.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Gary Russell
@@ -13,10 +16,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class RabbitMQTutorialsApplication {
+@RestController
+public class RabbitMQTutorialsApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RabbitMQTutorialsApplication.class, args);
     }
 
+    @RequestMapping("/")
+    String hello() {
+        return "Hello, Spring Boot !";
+    }
 }
